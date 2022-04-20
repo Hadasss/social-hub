71f34e42cc9ad96883fcafb0abac806e9bda15fb
Hadasss <hadas.gadish@gmail.com>
2022-04-20 14:03:35 -0700

    basic file structure + models

---
+++ models/Thought.js
@@ -0,0 +1,65 @@
+const { Schema, model, Types } = require("mongoose");
+
+const ReactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: "You must say something...",
+      max: 280,
+    },
+    username: {
+      type: String,
+      required: "You must identify yourself",
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      // TODO Use a getter method to format the timestamp on query
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: "You must think something...",
+      min: 1,
+      max: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      // TODO Use a getter method to format the timestamp on query
+    },
+    username: {
+      type: String,
+      required: "You must identify yourself",
+    },
+    reactions: [ReactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+      id: false,
+    },
+  }
+);
+
+ThoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model("Thought", ThoughtSchema);
+
+module.exports = Thought;
